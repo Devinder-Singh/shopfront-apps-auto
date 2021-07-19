8519ddbe32f240f6b04a4f5efd556b16be4c0f87
Kwezi Mayikana <61681165+Kmayikana@users.noreply.github.com>
2021-07-19 09:13:01 +0200

    Kwezi.ret 718.send.stats.to.grafana (#5)
    * added listener for sending stats to report portal
    
    * changed listener to python module
    
    * added docstring
    
    Co-authored-by: jasonbiel <jason.biel@takealot.com>
    Co-authored-by: kmayika <kwezimay@gmail.com>

---
+++ listeners/data/constants.py
@@ -0,0 +1,6 @@
+
+stats_mapping = {
+    "Search and Buy Item on Delivery using Credit Card": "critical.delivery1",
+    "Buy Daily Deal Item on Delivery using Credit Card": "critical.delivery2",
+    "Search and Buy Item on Delivery using Payfast and Create new Address": "critical.delivery3"
+}
