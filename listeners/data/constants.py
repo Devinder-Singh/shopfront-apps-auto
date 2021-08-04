1152b5910216a6bc2c383d5ef753191145faf749
Kwezi Mayikana <61681165+Kmayikana@users.noreply.github.com>
2021-08-04 09:12:05 +0200

    dynamically create metric path, removed constants file (#8)

--- listeners/data/constants.py
+++
@@ -1,6 +0,0 @@
-
-stats_mapping = {
-    "Search and Buy Item on Delivery using Credit Card": "critical.delivery1",
-    "Buy Daily Deal Item on Delivery using Credit Card": "critical.delivery2",
-    "Search and Buy Item on Delivery using Payfast and Create new Address": "critical.delivery3"
-}
